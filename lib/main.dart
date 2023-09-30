d5433959be447839a685ab8bfa570f1fc2ac652b
Cromuel Oliver <cromuelbarut@gmail.com>
2023-10-01 00:44:50 +0800

    init app

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:number_trivia/number_trivia_app.dart';
+
+void main() {
+  runApp(const NumberTriviaApp());
+}
---
+++ lib/number_trivia_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NumberTriviaApp extends StatelessWidget {
+  const NumberTriviaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Number Trivia',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('Number Trivia App')),
+      ),
+    );
+  }
+}
