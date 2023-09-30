f38c12de855f7253901c1fb31593c310ded1bce5
Cromuel Oliver <cromuelbarut@gmail.com>
2023-10-01 01:05:13 +0800

    created number trivia repository

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,10 @@
+import 'package:fpdart/fpdart.dart';
+
+import 'package:number_trivia/core/error/failure.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia_entity.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
