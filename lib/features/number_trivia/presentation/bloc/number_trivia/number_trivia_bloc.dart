6ade60d9c99323e2167a7225389cece6f4ced89a
Cromuel Oliver <cromuelbarut@gmail.com>
2023-10-14 18:01:19 +0800

    created test for "NumberTriviaBloc NumberTriviaState should be initial state"

--- lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
@@ -2,13 +2,27 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia_entity.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia_usecase.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia_usecase.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+  final GetConcreteNumberTrivia _getConcreteNumberTrivia;
+  final GetRandomNumberTrivia _getRandomNumberTrivia;
+  final InputConverter _inputConverter;
+
+  NumberTriviaBloc({
+    required GetConcreteNumberTrivia getConcreteNumberTrivia,
+    required GetRandomNumberTrivia getRandomNumberTrivia,
+    required InputConverter inputConverter,
+  })  : _getConcreteNumberTrivia = getConcreteNumberTrivia,
+        _getRandomNumberTrivia = getRandomNumberTrivia,
+        _inputConverter = inputConverter,
+        super(NumberTriviaInitial()) {
     on<NumberTriviaEvent>((event, emit) {
       // TODO: implement event handler
     });
---
+++ test/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc_test.dart
@@ -0,0 +1,43 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:number_trivia/core/util/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia_usecase.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia_usecase.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart';
+
+class MockGetConcreteNumberTrivia extends Mock
+    implements GetConcreteNumberTrivia {}
+
+class MockGetRandomNumberTrivia extends Mock implements GetRandomNumberTrivia {}
+
+class MockInputConverter extends Mock implements InputConverter {}
+
+void main() {
+  late MockGetConcreteNumberTrivia mockGetConcreteNumberTrivia;
+  late MockGetRandomNumberTrivia mockGetRandomNumberTrivia;
+  late MockInputConverter mockInputConverter;
+  late NumberTriviaBloc bloc;
+
+  setUp(() {
+    mockGetConcreteNumberTrivia = MockGetConcreteNumberTrivia();
+    mockGetRandomNumberTrivia = MockGetRandomNumberTrivia();
+    mockInputConverter = MockInputConverter();
+    bloc = NumberTriviaBloc(
+      getConcreteNumberTrivia: mockGetConcreteNumberTrivia,
+      getRandomNumberTrivia: mockGetRandomNumberTrivia,
+      inputConverter: mockInputConverter,
+    );
+  });
+
+  test(
+    'NumberTriviaState should be initial state',
+    () async {
+      // act
+      final result = bloc.state;
+      // assert
+      expect(result, NumberTriviaInitial());
+    },
+  );
+}
